91d38d7de56a20246f3d14fe39c0ef775276e19c
IZLuisZI <leduardo0905@gmail.com>
2024-08-06 16:55:27 -0400

    refactor: Update Astro configuration to include robots.txt policy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
+  integrations: [
+    tailwind(),
+    robotsTxt({
+      policy: [
+        {
+          userAgent: "*",
+          allow: "/",
+        },
+      ],
+    }),
+  ],
   site: "https://izluiszi.github.io/",
 });
