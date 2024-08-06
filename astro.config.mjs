f7dc50df9c8af2dea5307f8bd407d8291690f777
IZLuisZI <leduardo0905@gmail.com>
2024-08-06 16:59:21 -0400

    feat: Add sitemap generation to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
+import sitemap from "astro-sitemap";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,6 +14,10 @@ export default defineConfig({
           allow: "/",
         },
       ],
+    }),
+    sitemap({
+      filter: (page) => !page.includes("/404"), // Optional: Exclude 404 page from sitemap
+      hostname: "https://izluiszi.github.io/", // Replace with your actual site URL
     }),
   ],
   site: "https://izluiszi.github.io/",
